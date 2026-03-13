57f9715d0bc311f89361ab0417ff958b28c398ff
SkyMinus <minus_rav@qq.com>
2026-03-13 15:29:25 +0800

    firmware-builder: build: Initial board config for armsom_w3

---
+++ device/armsom/w3/armsom_w3.mk
@@ -0,0 +1,53 @@
+# ==============================================================================
+# Device Configuration: ArmSoM W3 (RK3588)
+#
+# Board Info:
+# Platform:
+# ........
+# ==============================================================================
+
+# Board Identity
+BOARD_NAME="ArmSoM W3"
+TARGET_BOARD="armsom-w3"
+SOC_FAMILY="rockchip"
+SOC="rk3588"
+TARGET_ARCH="arm64"
+
+# U-Boot
+BOOTLOADER="u-boot"
+UBOOT_IMPL="mainline"
+UBOOT_DEFCONFIG="w3-rk3588_defconfig"
+
+# Kernel
+KERNEL_IMPL="mainline"
+KERNEL_BASE_DEFCONFIG="defconfig"
+KERNEL_CONFIG_FRAGMENT="${DEVICE_DIR}/armsom/w3/kernel.config"
+KERNEL_IMAGE_NAME="Image"
+KERNEL_DTB="rockchip/rk3588-armsom-w3.dtb"
+
+# Kernel Command Line
+SERIAL_CONSOLE="ttyS2,1500000n8"
+EARLYCON="uart8250,mmio32,0xfeb50000"
+ROOTFS_PARTUUID="b921b045-1d" 
+KERNEL_CMDLINE_BASE="rootwait rw splash quiet loglevel=4"
+KERNEL_CMDLINE_DEBUG="console=${SERIAL_CONSOLE} earlycon=${EARLYCON}"
+KERNEL_CMDLINE_ROOT="root=PARTUUID=${ROOTFS_PARTUUID}"
+KERNEL_CMDLINE="${KERNEL_CMDLINE_ROOT} ${KERNEL_CMDLINE_BASE} ${KERNEL_CMDLINE_DEBUG}"
+
+# Partition & Image Layout
+IMAGE_BASENAME="${TARGET_BOARD}-${SUITE}-preinstall"
+IMAGE_NAME="${IMAGE_BASENAME}.img"
+IMAGE_SIZE_GIB="8"
+
+PARTITION_TABLE="gpt"
+BOOT_FS_TYPE="vfat"
+ROOTFS_FS_TYPE="ext4"
+
+IMAGE_START_MIB="32"
+BOOT_SIZE_MIB="512"
+BOOT_PART_START_MIB="32"
+BOOT_PART_END_MIB="544"
+ROOTFS_PART_START_MIB="544"
+
+ROCKCHIP_IDBLOADER_SECTOR="64"
+ROCKCHIP_UBOOT_ITB_SECTOR="16384"
