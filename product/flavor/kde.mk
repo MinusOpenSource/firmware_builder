432b4744062d5c291afd94962a11dc4b6fb7b738
SkyMinus <minus_rav@qq.com>
2026-04-02 14:56:25 +0800

    product: Initial suite flavors

---
+++ product/flavor/kde.mk
@@ -0,0 +1,5 @@
+FLAVOR="kde"
+ROOTFS_TYPE="desktop"
+DESKTOP_ENV="kde"
+ROOTFS_LOCAL_PACKAGES="platform-kubuntu"
+ROOTFS_SEEDED_SNAPS=$'snapd/classic=stable\ncore24/classic=stable'
---
+++ product/suite/resolute.mk
@@ -0,0 +1,7 @@
+SUITE="resolute"
+
+ROOTFS_RELEASE="26.04"
+ROOTFS_CODENAME="resolute"
+ROOTFS_ARCH="${ARCH}"
+ROOTFS_MIRROR="https://mirrors.ustc.edu.cn"
+ROOTFS_PORTS_MIRROR="${ROOTFS_MIRROR}/ubuntu-ports"
